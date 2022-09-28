0aaf827d2bb8c56dec06279062b4d108c2db9f97
Iury Flores <iury.flores@gmail.com>
2022-09-27 22:04:58 -0300

    Create pages

---
+++ src/pages/MoodEnhancer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MoodEnhancer = () => {
+  return (
+    <div>MoodEnhancer</div>
+  )
+}
---
+++ src/pages/MoodResult.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MoodResult = () => {
+  return (
+    <div>MoodResult</div>
+  )
+}
