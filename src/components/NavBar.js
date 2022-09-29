244efe2f6a567d50c02bb0735ba382c9d35e931a
Anneliese Kappey <anneliesekappey@gmail.com>
2022-09-29 20:54:16 -0300

    create navbar and render it in app.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const NavBar = (props) => {
+  return (
+    <div>
+      <img src="logo.png" alt="logo" />
+      <Link to="/"> Home </Link>
+    </div>
+  );
+};
