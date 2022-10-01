4ec3b4e51207fdf4626a6401cfd2547361cbbef6
Anneliese Kappey <anneliesekappey@gmail.com>
2022-10-01 16:52:10 -0300

    create questions for quiz, buttons not functional

--- src/pages/MoodQuiz.js
+++ src/pages/MoodQuiz.js
@@ -1,7 +1,113 @@
 import React from 'react'
 
 export const MoodQuiz = () => {
-  return <div>MoodQuiz</div>
+  return (
+    <div>
+      MoodQuiz
+      <div>
+        <h3> If you could do anything right now, you would... </h3>
+        <button className="hopeless/2">
+          take the midnight train going anywhere
+        </button>
+        <button className="excited/7"> go out drinking and dancing </button>
+        <button className="sad/0"> sleep for days </button>
+        <button className="angry/5"> set the house on fire </button>
+        <button className="peaceful/6">
+          move to the Himalayas to meditate
+        </button>
+      </div>
+      <div>
+        <h3> What would you like to eat right now? </h3>
+        <button className="angry/5"> Hurtfully spicy Mexican food </button>
+        <button className="hopeful/9">
+          A massive salad with all the fixings
+        </button>
+        <button className="bored/4">
+          An entire pizza and a tub of ice cream
+        </button>
+        <button className="happy/10">
+          A 4-course gourmet meal at my favorite restaurant
+        </button>
+        <button className="melancholic/3"> Nothing </button>
+      </div>
+      <div>
+        <h3> Thinking about the future, you see... </h3>
+        <button className="hopeless/2"> What future? </button>
+        <button className="sad/0"> Mass famine and destruction </button>
+        <button className="joyous/8"> Loads of time with people I love </button>
+        <button className="hopeful/9"> World travel </button>
+        <button className="bored/4"> More of the same </button>
+      </div>
+      <div>
+        <h3> Your heroes are: </h3>
+        <button className="hopeless/2"> all dead </button>
+        <button className="hopeful/9">
+          people who actively practice what they preach
+        </button>
+        <button className="peaceful/6"> anyone doing the best they can </button>
+        <button className="excited/7">
+          non-binary pansexual vegan leftist feminists
+        </button>
+        <button className="melancholic/3"> my mom </button>
+      </div>
+      <div>
+        <h3> How do you think you move through the world every day? </h3>
+        <button className="happy/10"> As joyfully as I can, until </button>
+        <button className="bored/4"> I don't know, man </button>
+        <button className="melancholic/3">
+          With as little effort as possible
+        </button>
+        <button className="hopeful/9"> Like a good person </button>
+        <button className="peaceful/6">
+          Always thinking about others and the impact I have on them
+        </button>
+        <div>
+          <h3> You can have endless amounts of one thing. You choose: </h3>
+          <button className="happy/10"> Love </button>
+          <button className="bored/4"> Money </button>
+          <button className="sad/0"> Intelligence </button>
+          <button className="hopeful/9"> Joy </button>
+          <button className="hopeless/2"> Sleep </button>
+        </div>
+        <div>
+          <h3>You can solve one world problem forever. Which do you solve?</h3>
+          <button className="peaceful/6"> Hunger </button>
+          <button className="melancholic/3"> Climate change </button>
+          <button className="angry/5"> Nah, let it burn </button>
+          <button className="sad/0"> Endless war </button>
+          <button className="hopeful/9"> Cancer </button>
+        </div>
+        <div>
+          <h3> What's a perfect day? </h3>
+          <button className="melancholic/3">
+            Sangrias in the park, later when it gets dark we go home
+          </button>
+          <button className="bored/4">When I don't have to put on pants</button>
+          <button className="happy/10">Seeing my friends and having fun</button>
+          <button className="joyous/8">
+            Meditating, exercising, cooking, reading, listening to music
+          </button>
+          <button className="sad/0"> Any day when I don't have to work </button>
+        </div>
+        <div>
+          <h3> How do you feel most days? </h3>
+          <button className="hopeless/2"> Meh </button>
+          <button className="excited/7"> Awesome </button>
+          <button className="hopeful/9"> Excited for the future </button>
+          <button className="sad/0"> Tired </button>
+          <button className="bored/4"> Hungry </button>
+        </div>
+        <div>
+          <h3> What's your favorite thing to say? </h3>
+          <button className="hopeful/9"> It'll all work out in the end </button>
+          <button className="angry/5"> What the fuck? </button>
+          <button className="peaceful/6"> It is what it is </button>
+          <button className="excited/7"> Fuck yeah, let's do it! </button>
+          <button className="hopeless/2"> Just leave it alone </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 const moodValue = [
