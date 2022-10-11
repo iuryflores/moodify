ae4c25774a71b26ac37350c2c0391ee6aa890731
Iury Flores <iury.flores@gmail.com>
2022-10-11 20:48:13 -0300

    Fix move getSetMedia function to inside useEffect callback

--- src/pages/FilteredLists.js
+++ src/pages/FilteredLists.js
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export const FilteredLists = () => {
-  const [songs, setSongs] = useState([])
-  const [movies, setMovies] = useState([])
-  const { mood } = useParams()
-
-  const getSetMedia = async () => {
-    const { data } = await axios.get('https://ironrest.herokuapp.com/moodify')
-    const filteredMoviesArray = data
-      .filter((media) => {
-        return media.mood.includes(mood.toLowerCase()) && media.type === 'movie'
-      })
-      .sort(() => 0.5 - Math.random())
-    setMovies(filteredMoviesArray.slice(0, 3))
-    const filteredSongsArray = data
-      .filter((media) => {
-        return media.mood.includes(mood.toLowerCase()) && media.type === 'song'
-      })
-      .sort(() => 0.5 - Math.random())
-    setSongs(filteredSongsArray.slice(0, 3))
-  }
+  const [songs, setSongs] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const { mood } = useParams();
 
   useEffect(() => {
-    getSetMedia()
-  }, [getSetMedia])
+    const getSetMedia = async () => {
+      const { data } = await axios.get(
+        "https://ironrest.herokuapp.com/moodify"
+      );
+      const filteredMoviesArray = data
+        .filter((media) => {
+          return (
+            media.mood.includes(mood.toLowerCase()) && media.type === "movie"
+          );
+        })
+        .sort(() => 0.5 - Math.random());
+      setMovies(filteredMoviesArray.slice(0, 3));
+      const filteredSongsArray = data
+        .filter((media) => {
+          return (
+            media.mood.includes(mood.toLowerCase()) && media.type === "song"
+          );
+        })
+        .sort(() => 0.5 - Math.random());
+      setSongs(filteredSongsArray.slice(0, 3));
+    };
+    getSetMedia();
+  }, []);
   return (
     <div>
       {songs.map((song) => (
@@ -35,5 +40,5 @@ export const FilteredLists = () => {
         <p>{movie.title}</p>
       ))}
     </div>
-  )
-}
+  );
+};
