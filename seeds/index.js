edb351882bf17272a1c2eca5643c8156edcaafb0
Iury Flores <iury.flores@gmail.com>
2022-10-13 22:33:44 -0300

    Fix src image

--- seeds/index.js
+++ seeds/index.js
@@ -2,5 +2,5 @@ const data = require("./data.json");
 const axios = require("axios");
 
 for(movie of data){
-    axios.post('https://ironrest.herokuapp.com/moodify', movie)
+    axios.put(`https://ironrest.herokuapp.com/moodify/${movie._id}`, {image:movie.image})
 }
