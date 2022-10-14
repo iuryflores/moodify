bac785d8cba27075cae4c5cc56eb843788eb3a5e
Anneliese Kappey <anneliesekappey@gmail.com>
2022-10-13 21:45:25 -0300

    fix moodquiz

--- src/pages/QuestionList.js
+++ src/pages/QuestionList.js
@@ -2,13 +2,14 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { QuestionCard } from '../components/QuestionCard'
 import { Button, LinkHome } from '../components/Shared'
+import { useNavigate } from 'react-router-dom'
 
 export const QuestionList = () => {
   const [questions, setQuestions] = useState([])
+  const [currentQuestion, setCurrentQuestion] = useState(0)
   let [getValue, setGetValue] = useState(0)
 
   const getQuestions = async () => {
-    // let currentQuestion = questions.shift()
     const { data } = await axios.get(
       'https://ironrest.herokuapp.com/MoodifyQuestions'
     )
@@ -24,45 +25,51 @@ export const QuestionList = () => {
     console.log(getValue)
   }
 
-  let moodType
+  const navigate = useNavigate()
 
-  if (getValue < 10) {
-    moodType = 'Sad'
-  } else if (getValue >= 10 && getValue < 20) {
-    moodType = 'Hopeless'
-  } else if (getValue >= 20 && getValue < 30) {
-    moodType = 'Melancholic'
-  } else if (getValue >= 30 && getValue < 40) {
-    moodType = 'Bored'
-  } else if (getValue >= 40 && getValue < 50) {
-    moodType = 'Angry'
-  } else if (getValue >= 50 && getValue < 60) {
-    moodType = 'Peaceful'
-  } else if (getValue >= 60 && getValue < 70) {
-    moodType = 'Excited'
-  } else if (getValue >= 70 && getValue < 80) {
-    moodType = 'Joyous'
-  } else if (getValue >= 80 && getValue < 90) {
-    moodType = 'Hopeful'
-  } else {
-    moodType = 'Happy'
+  const handleClick = () => {
+    if (currentQuestion === questions.length - 1) {
+      let moodType
+
+      if (getValue < 10) {
+        moodType = 'Sad'
+      } else if (getValue >= 10 && getValue < 20) {
+        moodType = 'Hopeless'
+      } else if (getValue >= 20 && getValue < 30) {
+        moodType = 'Melancholic'
+      } else if (getValue >= 30 && getValue < 40) {
+        moodType = 'Bored'
+      } else if (getValue >= 40 && getValue < 50) {
+        moodType = 'Angry'
+      } else if (getValue >= 50 && getValue < 60) {
+        moodType = 'Peaceful'
+      } else if (getValue >= 60 && getValue < 70) {
+        moodType = 'Excited'
+      } else if (getValue >= 70 && getValue < 80) {
+        moodType = 'Joyous'
+      } else if (getValue >= 80 && getValue < 90) {
+        moodType = 'Hopeful'
+      } else {
+        moodType = 'Happy'
+      }
+      navigate(`/mood-choice/${moodType}`)
+    }
+    setCurrentQuestion(currentQuestion + 1)
   }
 
   return (
     <div>
-      {questions
-        .map((question) => (
-          <QuestionCard
-            key={question._id}
-            {...question}
-            addedAnswerValue={addedAnswerValue}
-          />
-        ))
-        .shift()}
-      <Button> Next Question </Button>
-      <LinkHome to={`/mood-result/${moodType}`} key={moodType}>
-        Quiz Result!
-      </LinkHome>
+      {questions.length && currentQuestion <= questions.length - 1 && (
+        <QuestionCard
+          {...questions[currentQuestion]}
+          addedAnswerValue={addedAnswerValue}
+        />
+      )}
+      <Button onClick={() => handleClick()}>
+        {currentQuestion === questions.length - 1
+          ? 'Show Result'
+          : 'Next Question'}
+      </Button>
     </div>
   )
 }
