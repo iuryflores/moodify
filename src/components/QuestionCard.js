becc9a8d6623b720381a6e5204b3bc8a6881e1c2
Iury Flores <iury.flores@gmail.com>
2022-10-15 09:28:46 -0300

    Fix color change when answer is clicked

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -6,7 +6,13 @@ export const QuestionCard = ({
   questionText,
   answerOptions,
   addedAnswerValue,
+  isActive,
+  setIsActive,
 }) => {
+  const handleClickAnswer = () => {
+    setIsActive((current) => !current);
+  };
+
   return (
     <Card key={_id}>
       <p style={{ fontSize: "20px" }}>
@@ -14,36 +20,61 @@ export const QuestionCard = ({
       </p>
       <List>
         <ButtonAnswer
+          style={{
+            backgroundColor: isActive ? "salmon" : "",
+            color: isActive ? "white" : "",
+          }}
           onClick={() => {
             addedAnswerValue(answerOptions[0].value);
+            handleClickAnswer();
           }}
         >
           {answerOptions[0].answerText1}
         </ButtonAnswer>
         <ButtonAnswer
+          style={{
+            backgroundColor: isActive ? "salmon" : "",
+            color: isActive ? "white" : "",
+          }}
           onClick={() => {
             addedAnswerValue(answerOptions[1].value);
+            handleClickAnswer();
           }}
         >
           {answerOptions[1].answerText2}
         </ButtonAnswer>
         <ButtonAnswer
+          style={{
+            backgroundColor: isActive ? "salmon" : "",
+            color: isActive ? "white" : "",
+          }}
           onClick={() => {
             addedAnswerValue(answerOptions[2].value);
+            handleClickAnswer();
           }}
         >
           {answerOptions[2].answerText3}
         </ButtonAnswer>
         <ButtonAnswer
+          style={{
+            backgroundColor: isActive ? "salmon" : "",
+            color: isActive ? "white" : "",
+          }}
           onClick={() => {
             addedAnswerValue(answerOptions[3].value);
+            handleClickAnswer();
           }}
         >
           {answerOptions[3].answerText4}
         </ButtonAnswer>
         <ButtonAnswer
+          style={{
+            backgroundColor: isActive ? "salmon" : "",
+            color: isActive ? "white" : "",
+          }}
           onClick={() => {
             addedAnswerValue(answerOptions[4].value);
+            handleClickAnswer();
           }}
         >
           {answerOptions[4].answerText5}
